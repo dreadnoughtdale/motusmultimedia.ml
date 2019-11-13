bc1d79d18db9eca3bce422a386c99d1f3174ad47
dreadnoughtdale <dreadnought.dale@gmail.com>
2019-11-13 19:52:12 +0800

    Update

--- wp-login.php
+++ wp-login.php
@@ -20,52 +20,74 @@
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
 		</head>
-	<body class="login login-action-login wp-core-ui  locale-en-us">
+	<body class="login no-js login-action-login wp-core-ui  locale-en-us">
+	<script type="text/javascript">
+		document.body.className = document.body.className.replace('no-js','js');
+	</script>
 		<div id="login">
 		<h1><a href="https://wordpress.org/">Powered by WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/wp-login.php" method="post">
-	<p>
-		<label for="user_login">Username or Email Address<br />
-		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
-	</p>
-	<p>
-		<label for="user_pass">Password<br />
-		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
-	</p>
-			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
-	<p class="submit">
-		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-				<input type="hidden" name="redirect_to" value="/wp-admin/" />
-					<input type="hidden" name="testcookie" value="1" />
-	</p>
-	</form>
+		<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+			<p>
+				<label for="user_login">Username or Email Address</label>
+				<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" />
+			</p>
 
-			<p id="nav">
-					<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
-				</p>
-	
-	<script type="text/javascript">
-	function wp_attempt_focus(){
-	setTimeout( function(){ try{
-			d = document.getElementById('user_login');
-				d.focus();
-	d.select();
-	} catch(e){}
-	}, 200);
-	}
+			<div class="user-pass-wrap">
+				<label for="user_pass">Password</label>
+				<div class="wp-pwd">
+					<input type="password" name="pwd" id="user_pass" class="input password-input" value="" size="20" />
+					<button type="button" class="button button-secondary wp-hide-pw hide-if-no-js" data-toggle="0" aria-label="Show password">
+						<span class="dashicons dashicons-visibility" aria-hidden="true"></span>
+					</button>
+				</div>
+			</div>
+						<p class="forgetmenot"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> <label for="rememberme">Remember Me</label></p>
+			<p class="submit">
+				<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+									<input type="hidden" name="redirect_to" value="/wp-admin/" />
+									<input type="hidden" name="testcookie" value="1" />
+			</p>
+		</form>
 
-			wp_attempt_focus();
-			if(typeof wpOnload=='function')wpOnload();
-			</script>
-
-			<p id="backtoblog"><a href="/">
-		&larr; Back to Motus Multimedia	</a></p>
-			
-	</div>
-
-	
-		<div class="clear"></div>
+					<p id="nav">
+									<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+								</p>
+					<script type="text/javascript">
+			function wp_attempt_focus() {setTimeout( function() {try {d = document.getElementById( "user_login" );d.focus(); d.select();} catch( er ) {}}, 200);}
+wp_attempt_focus();
+if ( typeof wpOnload === 'function' ) { wpOnload() }		</script>
+				<p id="backtoblog"><a href="/">
+		&larr; Back to Motus Multimedia		</a></p>
+			</div>
+	<script type='text/javascript' src='/wp-includes/js/jquery/jquery.js'></script>
+<script type='text/javascript' src='/wp-includes/js/jquery/jquery-migrate.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _zxcvbnSettings = {"src":"\/wp-includes\/js\/zxcvbn.min.js"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='/wp-includes/js/zxcvbn-async.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var pwsL10n = {"unknown":"Password strength unknown","short":"Very weak","bad":"Weak","good":"Medium","strong":"Strong","mismatch":"Mismatch"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='/wp-admin/js/password-strength-meter.min.js'></script>
+<script type='text/javascript' src='/wp-includes/js/underscore.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/motusmultimedia.ml\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='/wp-includes/js/wp-util.min.js'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var userProfileL10n = {"warn":"Your new password has not been saved.","warnWeak":"Confirm use of weak password","show":"Show","hide":"Hide","cancel":"Cancel","ariaShow":"Show password","ariaHide":"Hide password"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='/wp-admin/js/user-profile.min.js'></script>
+	<div class="clear"></div>
 	</body>
 	</html>
 	
